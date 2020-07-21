53be73db4f06dee15a519c49648775e78d646004
PabloC <pablo.coello@outlook.es>
2020-07-21 13:07:11 +0200

    Major update

--- flikr.py
+++ flikr.py
@@ -4,53 +4,65 @@ from collections import defaultdict
 import geopandas as gpd
 import pandas as pd
 import time
-
-api_key = u'29222bf718a8d96ccb100cc8b340fbff'
-api_secret = u'773ddbdab9c8fcb2'
-
-asnevesbbox = '-8.471084, 42.070502, -8.346844, 42.184293'
-guadarramabbox = '-4.002646,40.889620,-3.936707, 40.904501'
-sudoe =  "-11.811238,35.852175,8.369423,46.840323"
-
-flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
-
-num_pags=10
+from pymongo import MongoClient, GEOSPHERE
+import shapely.geometry
 
 
-matrix = pd.DataFrame()
-counter = 0
-for i in range(21,31):
-    print(i)
-    photos = flickr.photos.search(tags='nature, forest',
-                                bbox=sudoe,
-                                accuracy=12,
-                                has_geo=1,
-                                geo_context=2,
-                                extras='geo, views',
-                                page=i,
-                                per_page=500)
+with open('./conf.json', 'r') as f:
+    conf = json.load(f)
+
+flickr = flickrapi.FlickrAPI(
+    conf['api_key'], conf['api_secret'], format='parsed-json')
+
+client = MongoClient('localhost', 27017)
+db = client.geo
+collection = db.location
+collection.create_index([("geometry", GEOSPHERE)])
 
 
-    toret = defaultdict(list)
-    for row in photos['photos']['photo']:
-        toret['id'].append(row['id'])
-        toret['Title'].append(row['title'])
-        toret['views'].append(row['views'])
-        toret['latitude'].append(row['latitude'])
-        toret['longitude'].append(row['longitude'])
+try:
+    init = 1
+    counter = 0
+    page = 1
+    records = 0
+    while init > 0:
+        photos = flickr.photos.search(tags=conf['tags'],
+                                      bbox=conf['bbox'],
+                                      accuracy=12,
+                                      has_geo=1,
+                                      geo_context=2,
+                                      extras='geo, views, date_taken, owner_name',
+                                      page=10,
+                                      per_page=500)
 
-    df = pd.DataFrame(toret)
-    matrix = pd.concat([matrix, df])
-    counter += 1
-    if counter == 10:
-        matrix.to_excel('sudoe.xlsx')
-        #time.sleep(3650)
-        counter = 0
-        
+        toret = defaultdict(list)
+        for row in photos['photos']['photo']:
+            toret['id'].append(row['id'])
+            toret['date'].append(row['datetaken'])
+            toret['Title'].append(row['title'])
+            toret['owner'].append(row['owner'])
+            toret['owner_name'].append(row['owner_name'])
+            toret['views'].append(row['views'])
+            toret['latitude'].append(row['latitude'])
+            toret['longitude'].append(row['longitude'])
 
-gdf =gpd.GeoDataFrame(
-    matrix, geometry=gpd.points_from_xy(matrix.longitude.astype(float), matrix.latitude.astype(float)))
+        df = pd.DataFrame(toret)
+        gdf = gpd.GeoDataFrame(
+            matrix, geometry=gpd.points_from_xy(matrix.longitude.astype(float), matrix.latitude.astype(float)))
+        data = gdf.to_dict(orient='records')
+        collection.insert_many(data)
 
-gdf.to_file('fotos_sudoe.shp')
+        page += 1
+        init = len(photos['photos']['photo'])
+        counter += init
+        records += init
 
-matrix.longitude.astype(float)
+        print(records)
+
+        if counter > 2900:
+            print('waiting one hour')
+            time.sleep(3650)
+            counter = 0
+
+except KeyboardInterrupt:
+    client.close()
